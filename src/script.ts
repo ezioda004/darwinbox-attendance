85fb58b1bdb3bc64108ea510113c6513f0f5d80b
Supreet Singh <supreetsingh.247@gmail.com>
2023-07-31 14:44:57 +0530

    Added Approval functionality & bifurcated approval & request function (#1)
    * Added Approval functionality & bifurcated approval & request functions and calls
    
    * [Stupid bug] Fixed looping issue in process var
    
    * Fixed scope bug
    
    * Reverted username, password logic
    
    ---------
    
    Co-authored-by: Supreet Singh <supreetsingh@Supreets-MacBook-Pro.local>
    Co-authored-by: Supreet Singh <supreet.singh2@pw.live>

--- src/script.ts
+++ src/script.ts
@@ -1,24 +1,25 @@
 import * as readline from "node:readline/promises";
 import puppeteer from "puppeteer";
 
-
 class App {
 
     page!: puppeteer.Page;
     browser!: puppeteer.Browser;
 
-    constructor() {
-        this.main();
+    async requestAttendance() {
+        await this.getInputAndLogin();
+        await this.startRequestAttendance();
+        await this.cleanup();
     }
 
-    async main() {
-        const { userName, password } = await this.getUserInput();
-        const { browser, page } = await this.startBrowser();
-        this.browser = browser;
-        this.page = page;
-        await this.login(userName, password);
-        await this.attendance();
-        await this.cleanup();
+    async approveAttendance() {
+        try {
+            await this.getInputAndLogin();
+            await this.startApproveAttendance();
+            await this.cleanup();
+        } catch(e) {
+            console.error('Fatt gya bro', e);
+        }
     }
 
     async getUserInput() {
@@ -27,13 +28,15 @@ class App {
             output: process.stdout
         });
 
+        // const userName = 'supreet.singh2@pw.live'; 
         const userName = await rl.question("Enter your email: ");
+        // const password = 'potato';
         const password = await rl.question("Enter your password: ");
         return { userName, password };
     }
 
     async startBrowser() {
-        const browser = await puppeteer.launch({ headless: "new", defaultViewport: null, args: ['--start-maximized'] })
+        const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-maximized'] })
 
         const page = await browser.newPage();
 
@@ -42,7 +45,11 @@ class App {
         return { browser, page };
     }
 
-    async login(userName: string, password: string) {
+    async getInputAndLogin() {
+        const { userName, password } = await this.getUserInput();
+        const { browser, page } = await this.startBrowser();
+        this.browser = browser;
+        this.page = page;
         console.log("Logging in.")
         const usernameEl = await this.page.waitForSelector(".username-field");
         const passwordEl = await this.page.waitForSelector(".password-field");
@@ -61,7 +68,7 @@ class App {
         console.log("Logged in...")
     }
 
-    async attendance() {
+    async startRequestAttendance() {
         console.log("Finding absent days...");
 
         await Promise.all([
@@ -73,11 +80,6 @@ class App {
         await this.page.waitForSelector(".even");
 
         const areDaysLeft = await this.page.evaluate(async () => { // Runs in the browser context
-            function sleep(time: number) {
-                return new Promise((resolve) => {
-                    setTimeout(resolve, time);
-                });
-            }
 
             const attendanceColor = "#f44336";
             const alreadyRequestedColor = "#999999";
@@ -89,6 +91,12 @@ class App {
             
             if (absentDays.length === 0) { // base condition for recursion
                 return false;
+            }
+
+            function sleep(time: number) {
+                return new Promise((resolve) => {
+                    setTimeout(resolve, time);
+                });
             }
 
             for await (const day of absentDays) {
@@ -112,9 +120,32 @@ class App {
         console.log("Submitted attendance for a day...");
 
         if (areDaysLeft) {
-            await this.attendance();
+            await this.startRequestAttendance();
         }
         console.log("Marked attendance for all days...");
+    }
+
+    async startApproveAttendance() {
+        console.log('this scope', this);
+        await this.page.goto("https://pwhr.darwinbox.in/tasksApi/GetTasks", { waitUntil: "networkidle2" });
+        await this.page.waitForSelector(".requestDiv");
+        // await this.sleep(3000);
+        await this.page.evaluate(async () => { // Runs in the browser context
+            console.log("Finding pending approvals...");
+            const attendanceContainers = document.querySelectorAll('.requestDiv');
+            for(let attendance of attendanceContainers) {
+                const isAttendanceRequest = attendance.querySelectorAll('.reqType a')[0] as HTMLAnchorElement;
+                // isAttendanceRequest.innerText === 'Attendance Request';
+                if(isAttendanceRequest?.innerText === 'Attendance Request') {
+                    const approveBtns = attendance.querySelectorAll('.btn-secondary-approve');
+                    for(const approveBtn of approveBtns) {
+                        if (approveBtn instanceof HTMLAnchorElement) { 
+                            approveBtn.click();
+                        }
+                    }
+                }
+            }
+        });
     }
 
     async cleanup() {
@@ -126,5 +157,16 @@ class App {
 
 }
 
-
 const app = new App();
+
+const processVar = process.argv;
+console.log('processVar', processVar);
+processVar.forEach((value, index) => {
+    console.log('value', value, 'index', index);
+    if(processVar[index] === 'approve') {
+        app.approveAttendance(); // For approval
+    } else if(processVar[index] === 'request') {
+        app.requestAttendance(); // For requesting attendance
+    } 
+    console.log(index, value);
+});
